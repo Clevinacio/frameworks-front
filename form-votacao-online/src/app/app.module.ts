ecca15d094b49096ed2d151d31e6681b4e2f47f6
Cleverton <cleverton.bezerra@imd.ufrn.br>
2021-08-09 11:10:26 -0300

    Finaliza projeto angular

--- form-votacao-online/src/app/app.module.ts
+++ form-votacao-online/src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { CardVotacaoComponent } from './card-votacao/card-votacao.component';
 import { CabineComponent } from './cabine/cabine.component';
 import { ResultadoComponent } from './resultado/resultado.component';
+import { FormVoteComponent } from './form-vote/form-vote.component';
+import { ListVoteComponent } from './list-vote/list-vote.component';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CardVotacaoComponent,
-    CabineComponent,
-    ResultadoComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CardVotacaoComponent,
+        CabineComponent,
+        ResultadoComponent,
+        FormVoteComponent,
+        ListVoteComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        FormlyModule.forRoot({
+            validationMessages: [{ name: 'required', message: 'Campo obrigatório' }],
+            extras: { lazyRender: true }
+        }),
+        FormlyBootstrapModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ form-votacao-online/src/app/list-vote/list-vote.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListVoteComponent } from './list-vote.component';
+
+describe('ListVoteComponent', () => {
+  let component: ListVoteComponent;
+  let fixture: ComponentFixture<ListVoteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListVoteComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListVoteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
